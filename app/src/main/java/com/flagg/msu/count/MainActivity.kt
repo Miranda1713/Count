7b6303f86b8755dc766c65c0993bbb825066e8e0
mirandaflagg <mjf846s@missouristate.edu>
2024-02-03 17:55:46 -0600

    Tap Commit 2

--- app/src/main/java/com/flagg/msu/count/MainActivity.kt
+++ app/src/main/java/com/flagg/msu/count/MainActivity.kt
@@ -4,14 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
 
-    private lateinit var tapButton: Button
+    private lateinit var tap_button: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tapButton = findViewById(R.id.tap_button)
+        tap_button = findViewById(R.id.tap_button)
     }
-}
+
+    class Counter {
+        private var count: Int = 0
+        fun addCount() {
+            count++
+        }
+
+        fun getCount(): Int {
+            return count
+        }
+    }
+    
+}    
+
+        
