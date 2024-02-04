e9f72c5a547243b1348d4394f3b8194aa4580816
mirandaflagg <mjf846s@missouristate.edu>
2024-02-03 20:51:50 -0600

    Counter Commit

--- app/src/main/java/com/flagg/msu/count/MainActivity.kt
+++ app/src/main/java/com/flagg/msu/count/MainActivity.kt
@@ -3,30 +3,35 @@ package com.flagg.msu.count
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity: AppCompatActivity() {
-
     private lateinit var tap_button: Button
-
+    private lateinit var tap_count: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tap_button = findViewById(R.id.tap_button)
-    }
+        tap_count = findViewById(R.id.tap_count)
 
-    class Counter {
-        private var count: Int = 0
-        fun addCount() {
-            count++
+        class Counter {
+            private var count: Int = 0
+            fun addCount() {
+                count++
+            }
+            fun getCount(): Int {
+                return count
+            }
         }
 
-        fun getCount(): Int {
-            return count
+        tap_button.setOnClickListener {
+            Counter().addCount()
+            tap_count.text = Counter().getCount().toString()
         }
     }
-    
-}    
+}
+
 
         
